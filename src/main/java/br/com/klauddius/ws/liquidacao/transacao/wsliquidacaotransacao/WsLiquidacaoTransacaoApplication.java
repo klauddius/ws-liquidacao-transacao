eb0d06fec8806ba20a27422184a7897e18d7207e
Claudio Brito Moreno <klauddius@gmail.com>
2018-05-24 22:36:34 -0300

    Commit Inicial - Setup Application
    Adiciona os arquivos para o setup inicial do microserviço.

---
+++ src/main/java/br/com/klauddius/ws/liquidacao/transacao/wsliquidacaotransacao/WsLiquidacaoTransacaoApplication.java
@@ -0,0 +1,12 @@
+package br.com.klauddius.ws.liquidacao.transacao.wsliquidacaotransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsLiquidacaoTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsLiquidacaoTransacaoApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/klauddius/ws/liquidacao/transacao/wsliquidacaotransacao/WsLiquidacaoTransacaoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.klauddius.ws.liquidacao.transacao.wsliquidacaotransacao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsLiquidacaoTransacaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
